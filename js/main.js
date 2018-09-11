c627660d2fbcd5a0c46d9961c84eacb622096504
Jaclyn Flomen <JaclynFlomen@Jaclyns-MBP.fc.ca>
2018-09-11 14:36:33 -0400

    more changes

--- js/main.js
+++ js/main.js
@@ -3,24 +3,6 @@ $(document).ready(function() {
 
   'use strict';
 
-
-// ========================================================================= //
-  //  //NAVBAR SHOW - HIDE
-  // ========================================================================= //
-
-
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 200 ) {
-      $("#main-nav, #main-nav-subpage").slideDown(700);
-      $("#main-nav-subpage").removeClass('subpage-nav');
-    } else {
-      $("#main-nav").slideUp(700);
-      $("#main-nav-subpage").hide();
-      $("#main-nav-subpage").addClass('subpage-nav');
-    }
-  });
-  
   // ========================================================================= //
   //  // RESPONSIVE MENU
   // ========================================================================= //
@@ -28,7 +10,3 @@ $(document).ready(function() {
   $('.responsive').on('click', function(e) {
     $('.nav-menu').slideToggle();
   });
-
-
-// Call the functions
-  magnifPopup();
