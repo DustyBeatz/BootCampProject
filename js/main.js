970cc4121224a9e9fa1ba62e96417afae1346a59
Jaclyn Flomen <JaclynFlomen@Jaclyns-MBP.fc.ca>
2018-09-13 18:34:38 -0400

    more work

--- js/main.js
+++ js/main.js
@@ -1,46 +1,11 @@
 /*global $, jQuery, alert*/
-function() {
-
   'use strict';
 
-  // ========================================================================= //
-  //  // RESPONSIVE MENU
-  // ========================================================================= //
-'use strict';
-
-    // ========================================================================= //
-    //  // RESPONSIVE MENU
-    // ========================================================================= //
-
-    function toggle() {
-        var button1 = document.querySelector(".nav-menu")
-        if (button1.style.display === "none") {
-            button1.style.display = "block";
-        } else {
-            button1.style.display = "none";
-        }
-
-        console.log(toggle){
-
-        }
-
-    }
-
-    toggle.addEventListener("Click", ion-navicon-round, false)
-
-
-
-
-
-	var openNav = document.querySelector(".ion-navicon-round");
-  var Nav = document.querySelector(".nav-menu");
-
-
-  function init(Nav) {
-		console.log("from init");
-
-
-		openNav.addEventListener("click", Nav, false);
-
-
-		}
+  function myFunction() {
+      var x = document.getElementById("nav-menu");
+      if (x.className === "nav-menu") {
+          x.className += " responsive";
+      } else {
+          x.className = "nav-menu";
+      }
+  }
