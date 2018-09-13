01fc4abd996ff86d197013e884f31f243835ff1e
Jaclyn Flomen <JaclynFlomen@Jaclyns-MacBook-Pro.local>
2018-09-13 01:20:53 -0400

    working on toggle and more styling changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 /*global $, jQuery, alert*/
-$(document).ready(function() {
+function() {
 
   'use strict';
 
   // ========================================================================= //
   //  // RESPONSIVE MENU
   // ========================================================================= //
+'use strict';
 
-  $('.responsive').on('click', function(e) {
-    $('.nav-menu').slideToggle();
-  });
+    // ========================================================================= //
+    //  // RESPONSIVE MENU
+    // ========================================================================= //
+
+    function toggle() {
+        var button1 = document.querySelector(".nav")
+        if (button1.style.display === "none") {
+            button1.style.display = "block";
+        } else {
+            button1.style.display = "none";
+        }
+
+        console.log(toggle){
+
+        }
+
+    }
+
+    toggle.addEventListener("Click", button1, false)
