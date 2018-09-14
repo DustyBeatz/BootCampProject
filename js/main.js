bb5bfceadfce9fe1a1a5bf1c137bd0b68faa364b
Jaclyn Flomen <JaclynFlomen@Jaclyns-MacBook-Pro.local>
2018-09-14 13:52:08 -0400

    Changing videos for responsive

--- js/main.js
+++ js/main.js
@@ -32,3 +32,26 @@ $(document).ready(function() {
   function setPlaySpeed("fastForward") {
       vid.playbackRate = 2;
   }
+
+
+
+
+/*Mobile toggle*/
+
+var video1 = document.querySelector("#trailer");
+var video2 = document.querySelector("#trailer_mobile");
+
+video1.addEventListener("click", swapMov, false);
+video2.addEventListener("click", swapMov, false);
+}
+
+  function swapMov(evt){
+  			//console.log("From swapMov");
+  			if(evt.currentTarget.id==="trailer_mobile"){
+  				video.src="video/video_Mobile.mp4";
+  			}else{
+  				video.src="video/video_Desktop.mp4";
+  			}
+  			console.log(video.currentScr);
+  		}
+  	window.addEventListener("load", init, false);
