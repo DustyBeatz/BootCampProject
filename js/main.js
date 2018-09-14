44202761375e107d162a1c0815866a9bb3a8bd4b
Jaclyn Flomen <JaclynFlomen@Jaclyns-MBP.fc.ca>
2018-09-13 20:16:01 -0400

    still working

--- js/main.js
+++ js/main.js
@@ -1,27 +1,2 @@
 /*global $, jQuery, alert*/
   'use strict';
-
-
-
-    var navMenu = document.getElementById('#main_nav');
-    var button = document.getElementById('#button');
-
-    button.onclick = function(displayNav){
-
-    };
-
-button.addEventListener("click", displayNav, false);
-
-
-
-
-    var dropdowns = document.getElementsByClassName('.nav-menu')
-    var i;
-    for (i = 0; i < dropdowns.length; i++){
-      var openDropdown = dropdowns[i];
-      if
-    (openDropdown.classList.contains('show'))
-    {
-      openDropdown.classList.remove('show');
-    }
-    }
